7608ac1d0f6b3e2e0c5f35a4fd21a838d75d2f54
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-07-02 12:23:43 +0700

    FinalProgram

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array = new string[] { "454", "8941", "4898", "2+2", "Student" };
+﻿string[] array = new string[] { "454", "8941", "4898", "2+2", "Student", "git"};
 
 void PrintArray(string[] arr)
 {
@@ -8,4 +8,19 @@ void PrintArray(string[] arr)
     }
     Console.WriteLine();
 }
+PrintArray(array);
+void Formation(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+    }
+        else arr[i] = string.Empty;
+    }
+}
+Formation(array);
+Console.Write("Итог: ");
 PrintArray(array);
