29f913699a090bd443a384f0a2184813b132a300
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-07-02 12:21:22 +0700

    PrintArray

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] array = new string[] { "454", "8941", "4898", "2+2", "Student" };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
